731cf09004dc4f86008249d9f9a1ac185363b107
丁文超 <f33chobits@gmail.com>
2017-06-09 12:02:19 +0800

    Add timer to .commonModes

--- Sources/FSPagerView.swift
+++ Sources/FSPagerView.swift
@@ -522,6 +522,7 @@ open class FSPagerView: UIView,UICollectionViewDataSource,UICollectionViewDelega
             return
         }
         self.timer = Timer.scheduledTimer(timeInterval: TimeInterval(self.automaticSlidingInterval), target: self, selector: #selector(self.flipNext(sender:)), userInfo: nil, repeats: true)
+        RunLoop.current.add(self.timer!, forMode: .commonModes)
     }
     
     @objc
