b834a6020545707be695d6028443182073caedce
janlionly <janlionly@gmail.com>
2020-06-13 16:00:47 +0800

    docs: 修改swift-tool版本

--- Package.swift
+++ Package.swift
@@ -24,7 +24,8 @@
 //  THE SOFTWARE.
 
 
-// swift-tools-version:5.0
+// swift-tools-version:5.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
