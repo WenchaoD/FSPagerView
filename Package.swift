fc77890f23f9ee0953632517d19f539ce7bf3f3c
janlionly <janlionly@gmail.com>
2020-06-18 17:00:56 +0800

    fix: add tvOS version to fix SPM building error

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "FSPagerView",
+    platforms: [.tvOS(.v11), .iOS(.v9)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
