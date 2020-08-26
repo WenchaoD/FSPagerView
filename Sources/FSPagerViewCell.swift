4bb1982db75c3e4e829355b2d2fa5a889aba2b4c
janlionly <janlionly@gmail.com>
2020-08-26 17:43:31 +0800

    feat: add subtitle for cell

--- Sources/FSPagerViewCell.swift
+++ Sources/FSPagerViewCell.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 open class FSPagerViewCell: UICollectionViewCell {
-    
+
     /// Returns the label used for the main textual content of the pager view cell.
     @objc
     open var textLabel: UILabel? {
@@ -19,20 +19,44 @@ open class FSPagerViewCell: UICollectionViewCell {
         let view = UIView(frame: .zero)
         view.isUserInteractionEnabled = false
         view.backgroundColor = UIColor.black.withAlphaComponent(0.6)
-        
+
         let textLabel = UILabel(frame: .zero)
         textLabel.textColor = .white
         textLabel.font = UIFont.preferredFont(forTextStyle: .body)
         self.contentView.addSubview(view)
         textLabel.numberOfLines = 0
         view.addSubview(textLabel)
-        
+
         textLabel.addObserver(self, forKeyPath: "font", options: [.old,.new], context: kvoContext)
-        
+
         _textLabel = textLabel
         return textLabel
     }
     open var customTextLabelFrame: CGRect? = nil
+
+    @objc
+    open var subtextLabel: UILabel? {
+        if let _ = _subtextLabel {
+            return _subtextLabel
+        }
+        let view = UIView(frame: .zero)
+        view.isUserInteractionEnabled = false
+        view.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+
+        let textLabel = UILabel(frame: .zero)
+        textLabel.textColor = .white
+        textLabel.font = UIFont.preferredFont(forTextStyle: .body)
+        self.contentView.addSubview(view)
+        textLabel.numberOfLines = 0
+        view.addSubview(textLabel)
+
+        textLabel.addObserver(self, forKeyPath: "font", options: [.old,.new], context: kvoContext)
+
+        _subtextLabel = textLabel
+        return textLabel
+    }
+    open var customSubtextLabelFrame: CGRect? = nil
+
     /// Returns the image view of the pager view cell. Default is nil.
     @objc
     open var imageView: UIImageView? {
@@ -44,13 +68,14 @@ open class FSPagerViewCell: UICollectionViewCell {
         _imageView = imageView
         return imageView
     }
-    
+
     fileprivate weak var _textLabel: UILabel?
+    fileprivate weak var _subtextLabel: UILabel?
     fileprivate weak var _imageView: UIImageView?
-    
+
     fileprivate let kvoContext = UnsafeMutableRawPointer(bitPattern: 0)
     fileprivate let selectionColor = UIColor(white: 0.2, alpha: 0.2)
-    
+
     fileprivate weak var _selectedForegroundView: UIView?
     fileprivate var selectedForegroundView: UIView? {
         guard _selectedForegroundView == nil else {
@@ -64,7 +89,7 @@ open class FSPagerViewCell: UICollectionViewCell {
         _selectedForegroundView = view
         return view
     }
-    
+
     open override var isHighlighted: Bool {
         set {
             super.isHighlighted = newValue
@@ -78,7 +103,7 @@ open class FSPagerViewCell: UICollectionViewCell {
             return super.isHighlighted
         }
     }
-    
+
     open override var isSelected: Bool {
         set {
             super.isSelected = newValue
@@ -88,17 +113,17 @@ open class FSPagerViewCell: UICollectionViewCell {
             return super.isSelected
         }
     }
-    
+
     public override init(frame: CGRect) {
         super.init(frame: frame)
         commonInit()
     }
-    
+
     public required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         commonInit()
     }
-    
+
     fileprivate func commonInit() {
         self.contentView.backgroundColor = UIColor.clear
         self.backgroundColor = UIColor.clear
@@ -107,9 +132,12 @@ open class FSPagerViewCell: UICollectionViewCell {
         self.contentView.layer.shadowOpacity = 0.75
         self.contentView.layer.shadowOffset = .zero
     }
-    
+
     deinit {
         if let textLabel = _textLabel {
+            textLabel.removeObserver(self, forKeyPath: "font", context: kvoContext)
+        }
+        if let textLabel = _subtextLabel {
             textLabel.removeObserver(self, forKeyPath: "font", context: kvoContext)
         }
     }
@@ -136,12 +164,36 @@ open class FSPagerViewCell: UICollectionViewCell {
           }
       }
 
+      private func updateSubtextLabelLayout() {
+            if let textLabel = _subtextLabel {
+              if let fr = customSubtextLabelFrame {
+                textLabel.frame = fr
+              } else {
+                textLabel.superview!.frame = {
+                    var rect = self.contentView.bounds
+                    let height = (textLabel.font.pointSize*1.5) * CGFloat(textLabel.numberOfVisibleLines)
+                    rect.size.height = height
+                    rect.origin.y = self.contentView.frame.height-height-textLabel.bounds.height
+                    return rect
+                }()
+                textLabel.frame = {
+                    var rect = textLabel.superview!.bounds
+                    rect = rect.insetBy(dx: 8, dy: 0)
+                    rect.size.height -= 1
+                    rect.origin.y += 1
+                    return rect
+                }()
+              }
+            }
+        }
+
     override open func layoutSubviews() {
         super.layoutSubviews()
         if let imageView = _imageView {
             imageView.frame = self.contentView.bounds
         }
         updateTextLabelLayout()
+        updateSubtextLabelLayout()
         if let selectedForegroundView = _selectedForegroundView {
             selectedForegroundView.frame = self.contentView.bounds
         }
@@ -152,6 +204,7 @@ open class FSPagerViewCell: UICollectionViewCell {
             if keyPath == "font" {
                 self.setNeedsLayout()
                 updateTextLabelLayout()
+                updateSubtextLabelLayout()
             }
         } else {
             super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
